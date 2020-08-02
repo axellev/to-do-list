ab0e61ee30e775a96e6cfcc581283ef815829e7d
Axelle <48962081+axellev@users.noreply.github.com>
2020-08-02 21:03:51 +0200

    Added a new file display-to-do.py .
    To display the items unchecked

---
+++ display-to-do.py
@@ -0,0 +1,11 @@
+# Display the items at the status "to-do".
+from examples import todolist_0
+
+items = todolist_0["items"]
+todos = []
+
+for item in items:
+    if not item["is_done"]:
+        todos.append(item["description"])
+
+print(todos)
