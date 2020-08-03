611433504b097c73f040c5f39388ad0424bbf27a
Axelle <48962081+axellev@users.noreply.github.com>
2020-08-03 09:20:56 +0200

    I changed the values of the to-do list

--- examples.py
+++ examples.py
@@ -2,8 +2,8 @@
 todolist_0 = {
     "title": "courses",
     "items": [
-        {"id": 1, "description": "legumes", "is_done": False},
-        {"id": 3, "description": "fruits", "is_done": True},
-        {"id": 5, "description": "viande", "is_done": False},
+        {"id": 1, "description": "make an example list", "is_done": False},
+        {"id": 3, "description": "make a new git commit", "is_done": True},
+        {"id": 5, "description": "looping trough a dictionnary", "is_done": False},
     ]
 }
