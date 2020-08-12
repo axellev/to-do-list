247ee4bc2c3a08ed578eb3458aa9ffa64a706e61
Axelle <48962081+axellev@users.noreply.github.com>
2020-08-12 12:35:54 +0200

    I added items to my to-do list + changed the title

--- examples.py
+++ examples.py
@@ -1,9 +1,19 @@
 # An example to-do list.
 todolist_0 = {
-    "title": "courses",
+    "title": "Training final project",
     "items": [
-        {"id": 1, "description": "make an example list", "is_done": False},
+        {"id": 1, "description": "make an example list", "is_done": True},
         {"id": 3, "description": "make a new git commit", "is_done": True},
-        {"id": 5, "description": "looping trough a dictionnary", "is_done": False},
+        {"id": 5, "description": "looping trough a dictionnary", "is_done": True},
+        {"id": 7, "description": "read flask documentation", "is_done": True},
+        {"id": 2, "description": "install venv", "is_done": False},
+        {"id": 10, "description": "open my webpage made with flask", "is_done": False},
+        {"id": 4, "description": "print HTML from my file display.py","is_done": False},
+        {"id": 2, "description": "change display.one to print ID","is_done": False},
+        {"id": 8, "description": "sort list with key filter and lambda","is_done": False},
+        {"id": 14, "description": "clean code","is_done": False},
+        {"id": 12, "description": "complete howto.md","is_done": False},
+        {"id": 15, "description": "make a function that use todolist_0","is_done": False},
+        {"id": 11, "description": "commit to github","is_done": False},
     ]
 }
