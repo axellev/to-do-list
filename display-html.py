fb839ef3b3f70bd718831b87c84d307a01b65621
Axelle <48962081+axellev@users.noreply.github.com>
2020-08-15 15:16:22 +0200

    Added function. Printed HTML more neatly

--- display-html.py
+++ display-html.py
@@ -1,7 +1,7 @@
 from examples import todolist_0
 
 
-begin = """<!DOCTYPE html>
+html_beginning = """<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -12,21 +12,29 @@ begin = """<!DOCTYPE html>
 <body>
 <h1>Voici ma liste</h1>"""
 
-end = """
-</ul>
-</body>
+html_ending = """</body>
 </html>
 """
 
-if __name__ == "__main__":
-
-    print(begin)
-    print("<h2>" + todolist_0["title"] + "</h2>")   
-    print("<ul>")
-    for (i, item) in enumerate(todolist_0["items"]):
+def display_items(items):
+    for item in items:
         if item["is_done"]:
             s = " (DONE)"
         else:
             s = ""
         print("<li>" + str(item["id"]) + ". " + item["description"] + s + "</li>")
-    print(end)
+
+if __name__ == "__main__":
+    print(html_beginning)
+    print("<h2>" + todolist_0["title"] + "</h2>")   
+    print("<ul>")
+    display_items(todolist_0["items"])
+    print("</ul>")
+    print(html_ending)
+
+
+
+
+
+
+
