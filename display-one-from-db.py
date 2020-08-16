da1fa9ddf340839f7c3b0867480f4a52012418ab
Axelle <48962081+axellev@users.noreply.github.com>
2020-08-16 15:04:38 +0200

    This program displays just ONE item

---
+++ display-one-from-db.py
@@ -0,0 +1,40 @@
+import sqlite3
+import sys
+from display import display_items
+
+def dict_factory(cursor, row):
+    d = {}
+    for idx, col in enumerate(cursor.description):
+        d[col[0]] = row[idx]
+    return d
+
+# execute only if run as a script
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Veuillez donner un argument")
+        exit(1)
+
+    i = int(sys.argv[1])
+
+    if i <= 0:
+        print("Veuillez rentrer un nombre supérieur à zéro")
+        exit(1)
+
+    # connection to to db
+    conn = sqlite3.connect('example.db')
+    conn.row_factory = dict_factory
+
+    # used to interact with the db
+    cursor = conn.cursor()
+    args = (i,)
+    cursor.execute('''
+        SELECT * FROM items 
+        WHERE items.id=?;
+    ''', args)
+    items = cursor.fetchall()
+
+    if len(items) == 0:
+        print("Il n'y a pas d'item pour cet ID")
+        exit(1)
+
+    display_items(items)
