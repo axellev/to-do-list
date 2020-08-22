32821c2299c18075cf38ce563bd96d6df556a867
Axelle <48962081+axellev@users.noreply.github.com>
2020-08-22 12:37:30 +0200

    display_todo.py now uses the database.

--- display_todo.py
+++ display_todo.py
@@ -1,11 +1,42 @@
-# Filter and display the items at the status "to-do".
-from examples import todolist_0
+import sqlite3
+import sys
+from db_helpers import dict_factory
+from display_text import status_to_string, display_items
 
-items = todolist_0["items"]
-todos = []
+# execute only if run as a script
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Veuillez donner un argument")
+        exit(1)
 
-for item in items:
-    if not item["is_done"]:
-        todos.append(item)
+    i = int(sys.argv[1])
 
-print(todos)
+    if i <= 0:
+        print("Veuillez rentrer un nombre supérieur à zéro")
+        exit(1)
+
+    # connection to to db
+    conn = sqlite3.connect('example.db')
+    conn.row_factory = dict_factory
+
+    # used to interact with the db
+    cursor = conn.cursor()
+    args = (i,)
+    cursor.execute('''
+        SELECT items.id, items.description, items.status, todolists.title
+        FROM items, todolists
+        WHERE items.todolist=todolists.id
+          AND items.todolist=?
+          AND items.status='todo';
+    ''', args)
+    items = cursor.fetchall()
+
+    if len(items) == 0:
+        print("Il n'y a pas d'items pour cet ID de todolist")
+        exit(1)
+
+    # We're sure there is at least one item since we checked above.
+    # All of them have the same title.
+    print(items[0]["title"])
+    # calling function
+    display_items(items)
