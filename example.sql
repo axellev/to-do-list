5904326a0af39293475210456e43be00e083afd1
Axelle <48962081+axellev@users.noreply.github.com>
2020-08-26 21:12:57 +0200

    Add a 3rd to do list

--- example.sql
+++ example.sql
@@ -6,7 +6,8 @@ CREATE TABLE IF NOT EXISTS "todolists" (
 
 INSERT INTO todolists VALUES
   (1, 'Python mini programs'),
-  (2, 'Training final project');
+  (2, 'Training final project'),
+  (3, 'Improve To-do list app');
 
 CREATE TABLE IF NOT EXISTS "items" (
 	"id"	INTEGER NOT NULL UNIQUE,
@@ -29,8 +30,8 @@ CREATE TABLE IF NOT EXISTS "users" (
 );
 
 INSERT INTO items VALUES
-  (21, 'try DB browser','todo', 1),
-  (23, 'create connexion db in python','todo', 1),
+  (21, 'try DB browser', 'todo', 1),
+  (23, 'create connexion db in python', 'todo', 1),
   (28, 'display database in python program', 'todo', 1),
   (1, 'make an example list', 'done', 2),
   (3, 'make a new git commit', 'done', 2),
@@ -38,17 +39,21 @@ INSERT INTO items VALUES
   (7, 'read flask documentation', 'done', 2),
   (2, 'install venv', 'todo', 2),
   (10, 'open my webpage made with flask', 'todo', 2),
-  (4, 'print HTML from my file display.py','todo', 2),
-  (6, 'change display.one to print ID','todo', 2),
-  (8, 'sort list with key filter and lambda','todo', 2),
-  (14, 'clean code','todo', 2),
-  (12, 'complete howto.md','todo', 2),
-  (15, 'make a function that use todolist_0','todo', 2),
-  (20, 'commit it','todo', 1),
-  (35, 'delete items','todo', 1),
-  (18, 'update items','todo', 1);
+  (4, 'print HTML from my file display.py', 'todo', 2),
+  (6, 'change display.one to print ID', 'todo', 2),
+  (8, 'sort list with key filter and lambda', 'todo', 2),
+  (14, 'clean code', 'todo', 2),
+  (12, 'complete howto.md', 'todo', 2),
+  (15, 'make a function that use todolist_0', 'todo', 2),
+  (20, 'commit it', 'todo', 1),
+  (35, 'delete items', 'todo', 1),
+  (18, 'update items', 'todo', 1),
+  (36, 'style app with CSS', 'todo', 3),
+  (37, 'render an item number instead of the ID', 'todo', 3),
+  (38, 'add task to a particular user', 'todo', 3),
+  (39, 'transform app into Kanban Board', 'todo', 3);
 
 DELETE FROM sqlite_sequence;
 INSERT INTO sqlite_sequence VALUES
-  ('todolists', 2),
-  ('items', 28);
+  ('todolists', 3),
+  ('items', 39);
