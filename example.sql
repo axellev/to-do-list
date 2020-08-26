390c84e3569ac77eb7e1ce2c0a21e8d90bf5c4e5
Axelle <48962081+axellev@users.noreply.github.com>
2020-08-26 16:33:38 +0200

    Fix wrong column name, and comma.

--- example.sql
+++ example.sql
@@ -12,12 +12,12 @@ CREATE TABLE IF NOT EXISTS "items" (
 	"id"	INTEGER NOT NULL UNIQUE,
 	"description"	TEXT NOT NULL CHECK(length(description) >= 3),
 	"status"	TEXT NOT NULL DEFAULT todo,
-	"todolist" INTEGER NOT NULL,
+	"todolist"	INTEGER NOT NULL,
 	PRIMARY KEY("id" AUTOINCREMENT),
-  CONSTRAINT fk_todolists
-	FOREIGN KEY("todolist_id") 
-  REFERENCES todolists("id")
-  ON DELETE CASCADE
+	CONSTRAINT fk_todolists
+	FOREIGN KEY("todolist")
+	REFERENCES todolists("id")
+	ON DELETE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS "users" (
@@ -25,7 +25,7 @@ CREATE TABLE IF NOT EXISTS "users" (
   "username"	TEXT NOT NULL UNIQUE,
   "email"	TEXT NOT NULL UNIQUE,
   "hashed_password"	TEXT NOT NULL,
-  PRIMARY KEY("id" AUTOINCREMENT),
+  PRIMARY KEY("id" AUTOINCREMENT)
 );
 
 INSERT INTO items VALUES
